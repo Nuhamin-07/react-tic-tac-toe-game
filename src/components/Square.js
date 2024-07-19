d54d27898c85cb54a3091c6547e306786c70131a
Nuhamin-07 <nuhamin.gulilat.7@gmail.com>
2024-07-18 19:12:50 -0700

    react tic-tac-toe game

---
+++ src/components/Square.js
@@ -0,0 +1,6 @@
+import React, {useState} from "react"
+
+export default function Square({value, onSquareClick}) {
+
+    return <button className='square' onClick={onSquareClick}>{value}</button>
+}
